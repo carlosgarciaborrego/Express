84d18c036fd670b7adbb6e7a5fc92942f1746852
Carlos Jesus Garcia Borrego <carlosjesusgarciaborrego@gmail.com>
2020-06-10 11:11:21 +0200

    Terminado el curso de Express

--- ecommerce/routes/views/products.js
+++ ecommerce/routes/views/products.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const ProductsService = require("../../services/products");
+const { config } = require("../../config");
 
-const productsService = new ProductsService();
+const cacheResponse = require("../../utils/cacheResponse");
+const { FIVE_MINUTES_IN_SECONDS } = require("../../utils/time");
+
+const productService = new ProductsService();
 
 router.get("/", async function(req, res, next) {
-  const { tags }= req.query;
-
-  try{
-    const products = productsService.getProducts({ tags });
-    res.render("products", { products });
-  }catch(err){
+  cacheResponse(res, FIVE_MINUTES_IN_SECONDS);
+  const { tags } = req.query;
+  try {
+    const products = await productService.getProducts({ tags });
+    res.render("products", { products, dev: config.dev });
+  } catch (err) {
     next(err);
   }
- 
 });
 
 module.exports = router;
--- ecommerce/services/products.js
+++ ecommerce/services/products.js
@@ -1,33 +1,48 @@
-const productsMocks = require('../utils/mocks/products');
-const MongoLib = require('../lib/mongo');
+const productsMocks = require("../utils/mocks/products");
+const MongoLib = require("../lib/mongo");
 
 class ProductsService {
-    constructor(){
-        this.collection = 'products';
-        this.mongoDB = new MongoLib();
-    }
+  constructor() {
+    this.collection = "products";
+    this.mongoDB = new MongoLib();
+  }
 
-    async getProducts({ tags }){
-        const query = tags && { tags: { $in: tags }};
-        const products = await this.mongoDB.getAll(this.collection, query);
-        return products || [];
-    }
+  async getProducts({ tags }) {
+    const query = tags && { tags: { $in: tags } };
+    const products = await this.mongoDB.getAll(this.collection, query);
 
-    getProduct({ productId }){
-        return Promise.resolve(productsMocks[0]);
-    }
+    return products || [];
+  }
 
-    createProduct({ product }){
-        return Promise.resolve(productsMocks[0]);
-    }
+  async getProduct({ productId }) {
+    const product = await this.mongoDB.get(this.collection, productId);
+    return product || {};
+  }
 
-    updateProduct({ productId, product }){
-        return Promise.resolve(productsMocks[0]);
-    }
+  async createProduct({ product }) {
+    const createProductId = await this.mongoDB.create(this.collection, product);
 
-    deleteProduct({ productId }){
-        return Promise.resolve(productsMocks[0]);
-    }
+    return createProductId;
+  }
+
+  async updateProduct({ productId, product }) {
+    const updateProductId = await this.mongoDB.update(
+      this.collection,
+      productId,
+      product
+    );
+
+    return updateProductId;
+  }
+
+  async deleteProduct({ productId }) {
+    const deletedProductId = await this.mongoDB.delete(
+      this.collection,
+      productId
+    );
+
+    return deletedProductId;
+  }
 }
 
 module.exports = ProductsService;
